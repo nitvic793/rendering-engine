07ff9682d3baecb911c3a9fa9ce37fa13a107417
Jishnu Girish <jishnugirish@gmail.com>
2018-03-27 10:08:50 -0400

    fixed water lighting

--- RenderingEngine/PS_WaterShader.hlsl
+++ RenderingEngine/PS_WaterShader.hlsl
@@ -1,5 +1,5 @@
 
-#define MAX_LIGHTS 2
+#define MAX_LIGHTS 32
 struct VertexToPixel
 {
 	float4 position		: SV_POSITION;
@@ -14,12 +14,14 @@ struct DirectionalLight
 	float4 AmbientColor;
 	float4 DiffuseColor;
 	float3 Direction;
+	float Padding;
 };
 
 struct PointLight
 {
 	float4 Color;
 	float3 Position;
+	float Range;
 };
 
 cbuffer externalData : register(b0)
@@ -35,44 +37,6 @@ cbuffer externalData : register(b0)
 Texture2D diffuseTexture : register(t0);
 Texture2D normalTexture : register(t1);
 SamplerState basicSampler : register(s0);
-//
-//float4 calculateDirectionalLight(float3 normal, DirectionalLight light)
-//{
-//	float3 dirToLight = normalize(-light.Direction);
-//	float NdotL = dot(normal, dirToLight);
-//	NdotL = saturate(NdotL);
-//	return light.DiffuseColor * NdotL + light.AmbientColor;
-//}
-//
-//float calculateSpecular(float3 normal, float3 worldPos, float3 dirToPointLight, float3 camPos)
-//{
-//	float3 dirToCamera = normalize(camPos - worldPos);
-//	float3 refl = reflect(-dirToPointLight, normal);
-//	float specExp = 64;
-//	float spec = pow(saturate(dot(dirToCamera, refl)), specExp);
-//	return spec;
-//}
-//
-//float4 calculatePointLight(float3 normal, float3 worldPos, PointLight light)
-//{
-//	float3 dirToPointLight = normalize(light.Position - worldPos);
-//	float pointNdotL = dot(normal, dirToPointLight);
-//	pointNdotL = saturate(pointNdotL);
-//	float spec = calculateSpecular(normal, worldPos, dirToPointLight, cameraPosition);
-//	return spec + light.Color * pointNdotL;
-//}
-//
-//float3 calculateNormalFromMap(float2 uv, float3 normal, float3 tangent)
-//{
-//	float3 normalFromTexture = normalTexture.Sample(basicSampler, uv).xyz;
-//	float3 unpackedNormal = normalFromTexture * 2.0f - 1.0f;
-//	float3 N = normal;
-//	float3 T = normalize(tangent - N * dot(tangent, N));
-//	float3 B = cross(N, T);
-//	float3x3 TBN = float3x3(T, B, N);
-//	return normalize(mul(unpackedNormal, TBN));
-//	return unpackedNormal;
-//}
 
 Texture2D roughnessTexture : register(t2);
 // Range-based attenuation function
@@ -127,25 +91,7 @@ float3 calculateNormalFromMap(float2 uv, float3 normal, float3 tangent)
 }
 float4 main(VertexToPixel input) : SV_TARGET
 {
-	////........................................................................
-	////Translate the normal map
-	//input.uv.x += translate;
-
-	//input.normal = normalize(input.normal);
-	//float4 surfaceColor = diffuseTexture.Sample(basicSampler, input.uv);
-	////return surfaceColor;
-	//float3 finalNormal = calculateNormalFromMap(input.uv, input.normal, input.tangent);
-	//finalNormal = normalize(finalNormal);
-	//
-	//float4 dirLight = calculateDirectionalLight(finalNormal, light) * surfaceColor;
-	//float4 secDirLight = calculateDirectionalLight(finalNormal, secondaryLight) * surfaceColor;
-	//float4 pLight = calculatePointLight(finalNormal, input.worldPos, pointLight)  * surfaceColor;
-	//float3 pLightDir = normalize(pointLight.Position - input.worldPos);
-	//float spec = calculateSpecular(finalNormal, input.worldPos, pLightDir, cameraPosition);
-
-	//return dirLight;//+ secDirLight + spec.xxxx;
-	////.........................................................................
-	
+	input.uv.x += translate;
 	float4 surfaceColor = diffuseTexture.Sample(basicSampler, input.uv);
 	float3 finalNormal = calculateNormalFromMap(input.uv, input.normal, input.tangent);
 	input.normal = normalize(input.normal);
@@ -164,9 +110,5 @@ float4 main(VertexToPixel input) : SV_TARGET
 		totalColor += calculatePointLight(finalNormal, input.worldPos, pointLights[i], roughness)  * surfaceColor;
 	}
 
-	/*float4 dirLight = calculateDirectionalLight(finalNormal, dirLights[0]) * surfaceColor;
-	float4 secDirLight = calculateDirectionalLight(finalNormal, dirLights[1]) * surfaceColor;
-	float4 pLight = calculatePointLight(finalNormal, input.worldPos, pointLights[0])  * surfaceColor;*/
-	//return dirLight + secDirLight + pLight;
 	return totalColor;
 }
