0e175ddd83721cd1f8935b66f4d40bd13956010e
tomboyanushka <32500051+tomboyanushka@users.noreply.github.com>
2018-05-06 14:55:17 -0400

    threshold

--- RenderingEngine/LFThresholdPS.hlsl
+++ RenderingEngine/LFThresholdPS.hlsl
@@ -11,7 +11,7 @@ SamplerState Sampler	: register(s0);
 
 float4 main(VertexToPixel input) : SV_TARGET
 {
-	float Threshold = 0.99999;
+	float Threshold = 0.99999995;
 	float4 surfaceColor = Pixels.Sample(Sampler, input.uv);
 	float4 bright = (surfaceColor - Threshold) / (1 - Threshold);
 	surfaceColor = saturate(bright);
